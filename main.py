7afd91b786e7b3012017603631fd8c67341230dc
ohuallachain99 <darraghohuallachain@gmail.com>
2026-04-02 16:00:12 +0100

    environment setup

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    print("single quotes")
+
+    print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
