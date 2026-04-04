c019b86864bc7124ede123524d0314e53e5b2f2a
ohuallachain99 <darraghohuallachain@gmail.com>
2026-04-04 18:15:06 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from langchain_core import agents
+from langchain_ollama import ChatOllama
+from tavily import 
+
+load_dotenv()
+
+
+def main():
+    print("Hello from project-003-agents-under-the-hood!")
+
+    llm = ChatOllama(model="qwen2.5:7b", temperature=0)
+
+
+if __name__ == "__main__":
+    main()
+    main()
